9fe94ed161b4224f22d64076afa94bc722f4d6fa
Will-Answer <will.pinder@answerdigital.com>
2024-08-26 20:05:07 +0100

    Updated some docstrings and comments in marking.py and updated the README

--- README.md
+++ README.md
@@ -22,9 +22,15 @@
 Create a directory containing model solutions to your questions. These model solutions will be passed to the database to produce an answer set that can be compared against each candidate's results. Each question should have an individual .sql file, with its file name being the question number.  
 Edit the "requires_order" tag in settings.json to contain an array with the question numbers that require a specific order. By default, questions are marked without respect to the order of the table in the output, but this tag is passed to marking.py to let it know that those questions should be marked with order taken into consideration.  
 Create a directory that will store the candidate files. Each candidate should have their own folder that contains their .sql files. Each question in the responses should also have its own file, with filename [question number].sql, like in the template directory.  
-The output currently is just a log that informs of any errors. This will be updated later when marking.py is complete
+The score of each candidate is written to results/scorecard.txt as a pandas dataframe. Any mistakes are written to results/mistakes.txt for review of the query. Any erroneous queries are output to log.txt for review.
 
 ## --Troubleshooting--
 
-If init.cmd does not work, check the installation directory of Postgres. You might have to change the PATH commands.  
-If pip doesn't work, make sure it is in PATH in the system environment variables.  
+If init.cmd does not work, check the installation directory of Postgres. You might have to change the PATH commands. Alternatively, you could manually add it to PATH in the system environment variables if you have admin access, but I don't :(  
+If pip doesn't work, make sure it is in PATH in the system environment variables. You can do this through the installer. 
+If there are issues regarding INSERT or other non-select commands, the database reverts to its initial state when there is an error and at the start of each candidate's set of queries. This may cause issues if a query fails and other queries are dependent on it executing.  
+The way the database reverts is via the SAVEPOINT and ROLLBACK queries. ROLLBACK is unable to revert once COMMIT has been used, which could cause issues when the next candidate is processed. To undo any commits, init.cmd must be rerun.  
+
+## --Thanks--
+Thank you to Yamin and Harry for reviewing this project.  
+Thank you to everyone at Answer for being so welcoming.  
--- marking.py
+++ marking.py
@@ -6,7 +6,7 @@ import sys
 import json
 import os
 
-log = open('log.txt','a') #opens crashlog and database
+log = open('log.txt','a') #open logs and database
 db = dbc.Database()
 with open('settings.json') as settingsjson:
     settings = json.load(settingsjson)
@@ -32,7 +32,7 @@ def queryall(questions,type='t',candidate='tester'):
     Outputs the candidate, question and error to log if a response causes an error
     """
     processed = {}
-    db.rollback()
+    db.rollback() #ensure the database is in its default state
     for qnum in questions:
         try:
             response = db.query(questions[qnum])
@@ -57,8 +57,8 @@ def mark(template,responses,ordered=[]):
     - responses (dict) - the response dictionary of all of the canditates
     - ordered (list) - a list of the questions that should be ordered
     """
-    scores = {'name':['Total'],'score':[len(template)]}
-    candnum = 0
+    scores = {'name':['Total'],'score':[len(template)]} #used to track candidate scores
+    candnum = 0 #used to track position in the lists
     for candidate in responses:
         candnum += 1
         scores['name'].append(candidate)
@@ -68,23 +68,23 @@ def mark(template,responses,ordered=[]):
             res_table = responses[candidate][qnum]
             temp_table = template[qnum]
 
-            if type(res_table) == str:
+            if type(res_table) == str: #error output
                 if res_table == 'Err':
                     print(f'Candidate: {candidate}\nQuestion: {qnum}\nError, see log\n------------',file=mistakes)
 
-            elif res_table.size == 1:
+            elif res_table.size == 1: #handles single value outputs
                 if res_table.values == temp_table.values:
                     scores['score'][candnum] += 1
                 else:
                     incorrect(candidate,qnum)
             
-            elif len(res_table) == 1:
+            elif len(res_table) == 1: #handles single row outputs
                 if comparerow(temp_table[:],res_table[:]):
                     scores['score'][candnum] += 1
                 else:
                     incorrect(candidate,qnum)
             
-            elif int(qnum) in ordered:
+            elif int(qnum) in ordered: #handles multi-row ordered outputs
                 if res_table.shape[0] == temp_table.shape[0]:
                     if comparerows(temp_table,res_table):
                         scores['score'][candnum] += 1
@@ -93,7 +93,7 @@ def mark(template,responses,ordered=[]):
                 else:
                     incorrect(candidate,qnum)
             
-            else:
+            else: #handles multi-row unordered outputs. compares each response row to the rows in template
                 if res_table.shape[0] == temp_table.shape[0]:
                     equivalent = True
                     temp_dupe = temp_table[::1]
@@ -101,14 +101,14 @@ def mark(template,responses,ordered=[]):
                         found = False
                         for b in range(len(temp_table.index)):
                             try:
-                                if comparerow(temp_dupe[b:b],res_table[a:a]):
+                                if comparerow(temp_dupe[b:b],res_table[a:a]): #if a match is found, delete that row and go to the next row in response
                                     found = True
                                     temp_dupe.drop([b])
                                     break
-                            except IndexError:
-                                incorrect(candidate,qnum)
+                            except IndexError: #if no matching row is found, loop breaks and q is marked as incorrect 
+                                break
                         if not found:
-                            incorrect(candidate,qnum)
+                            equivalent = False
                             break
                     if equivalent:
                         scores['score'][candnum] += 1
@@ -119,6 +119,7 @@ def mark(template,responses,ordered=[]):
     return pd.DataFrame(scores)
 
 def comparerow(temp_row,res_row):
+    '''Compares a template row of a dataframe to a response'''
     res = list(res_row)
     try:
         for item in temp_row:
@@ -128,6 +129,7 @@ def comparerow(temp_row,res_row):
         return False
 
 def comparerows(temp_table,res_table):
+    '''Iterated comparerow() functions, taking a whole table instead of a single row'''
     comps = []
     for i in range(len(list(res_table.index))):
         try:
@@ -140,6 +142,7 @@ def comparerows(temp_table,res_table):
         return True
 
 def incorrect(candidate,qnum):
+    '''Outputs candidate name, question number and SQL query to mistakes file'''
     queryfile = open(f'{os.getenv('responses')}\\{candidate}\\{qnum}.sql')
     querylines = ''
     for line in queryfile:
@@ -147,16 +150,16 @@ def incorrect(candidate,qnum):
     print(f'Candidate: {candidate}\nQuestion: {qnum}\n Query:\n{querylines}\n--------------\n',file=mistakes)
     queryfile.close()
                     
+if __name__ == '__main__':
+    template = load.get_template() #loads template responses into a dictionary of pandas dataframes (keys are question numbers)
+    responses = load.get_responses() #loads responses into a dictionary of dictionaries of pandas dataframes (keys are candidates then quesiton numbers)
 
-template = load.get_template() #loads template responses into a dictionary of pandas dataframes (keys are question numbers)
-responses = load.get_responses() #loads responses into a dictionary of dictionaries of pandas dataframes (keys are candidates then quesiton numbers)
+    template_proc = queryall(template,'t') #processes template
+    response_proc = {}
+    for candidate in responses: #processes responses
+        response_proc[candidate] = queryall(responses[candidate],'r',candidate)
 
-template_proc = queryall(template,'t') #processes template
-response_proc = {}
-for candidate in responses: #processes responses
-    response_proc[candidate] = queryall(responses[candidate],'r',candidate)
-
-print(mark(template_proc,response_proc,settings['requires_order']),file=scorecard)
+    print(mark(template_proc,response_proc,settings['requires_order']),file=scorecard) #compares templates to responses
 '''for i in template_proc: #prints the processed templates and responses
     print(f'{i})\n{template_proc[i]}\n\n')
 
